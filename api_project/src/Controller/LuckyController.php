724f2ef501a116f4fdf9f65e6cc436047a12e1da
Oliver Mensah <olivermensah96@gmail.com>
2021-02-12 09:32:40 +0000

    using route anotation for controller routing

--- api_project/src/Controller/LuckyController.php
+++ api_project/src/Controller/LuckyController.php
@@ -5,9 +5,13 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController
 {
+     /**
+       * @Route("/lucky/number")
+     */
     public function number(): Response
     {
         $number = random_int(0, 100);
